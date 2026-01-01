f2f2195e05b75152cf257159071059dbab75424e
HYIND <1255231699@qq.com>
2026-01-02 07:48:59 +0800

    同步Libs

--- ThirdParty/publicShare/Coroutine.h
+++ ThirdParty/publicShare/Coroutine.h
@@ -296,50 +296,53 @@ inline Task<void> TaskPromise<void>::get_return_object() noexcept
         TaskPromiseBase<void>::_syncwaitcv);
 }
 
-template <typename U>
-struct is_task : std::false_type
+namespace CoroTask
 {
-};
-template <typename T>
-struct is_task<Task<T>> : std::true_type
-{
-};
-template <typename T>
-static constexpr bool is_task_v = is_task<T>::value;
-
-template <typename T>
-struct flatten_task
-{
-    using type = T;
-};
-
-template <typename T>
-struct flatten_task<Task<T>>
-{
-    using type = T; // 展开一层
-};
-
-template <typename T>
-using flatten_task_t = typename flatten_task<T>::type;
-
-template <typename Callable>
-static auto RunCoroTask(Callable callable)
-    -> Task<flatten_task_t<decltype(callable())>>
-{
-    using RawReturnType = decltype(callable());
-
-    if constexpr (is_task_v<RawReturnType>)
+    template <typename U>
+    struct is_task : std::false_type
     {
-        co_return co_await callable();
-    }
-    else if constexpr (std::is_void_v<RawReturnType>)
+    };
+    template <typename T>
+    struct is_task<Task<T>> : std::true_type
     {
-        callable();
-        co_return;
-    }
-    else
+    };
+    template <typename T>
+    static constexpr bool is_task_v = is_task<T>::value;
+
+    template <typename T>
+    struct flatten_task
     {
-        co_return callable();
+        using type = T;
+    };
+
+    template <typename T>
+    struct flatten_task<Task<T>>
+    {
+        using type = T; // 展开一层
+    };
+
+    template <typename T>
+    using flatten_task_t = typename flatten_task<T>::type;
+
+    template <typename Callable>
+    static auto Run(Callable callable)
+        -> Task<flatten_task_t<decltype(callable())>>
+    {
+        using RawReturnType = decltype(callable());
+
+        if constexpr (is_task_v<RawReturnType>)
+        {
+            co_return co_await callable();
+        }
+        else if constexpr (std::is_void_v<RawReturnType>)
+        {
+            callable();
+            co_return;
+        }
+        else
+        {
+            co_return callable();
+        }
     }
 }
 
