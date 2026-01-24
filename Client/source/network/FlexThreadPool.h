7fc4d75726b68f8da561da47144565c3dd53f69a
HYIND <1255231699@qq.com>
2026-01-25 07:21:00 +0800

    fix

--- Client/source/network/FlexThreadPool.h
+++ Client/source/network/FlexThreadPool.h
@@ -78,7 +78,7 @@ public:
 
             while (true)
             {
-                auto status = fut.wait_for(std::chrono::milliseconds(100));
+                auto status = fut.wait_for(std::chrono::milliseconds(10));
                 if (status == std::future_status::ready)
                 {
                     try
